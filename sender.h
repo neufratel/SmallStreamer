954fcf77f09d4cd57b95223b4f43a2901662f148
Rudzki Jarosław <rudzki.jaroslaw@gmail.com>
2015-06-23 09:02:17 +0200

    Adding first junk of code for client aps.

---
+++ sender.h
@@ -0,0 +1,25 @@
+#ifndef SENDER_H
+#define SENDER_H
+
+#include <QObject>
+#include <QUdpSocket>
+
+class Stream;
+class StreamSender : public QObject
+{
+    Q_OBJECT
+public:
+    explicit StreamSender(QObject *parent = 0);
+    void HelloUDP();
+   	void sendStream(Stream s);
+signals:
+    
+public slots:
+    void readyRead();
+
+private:
+    QUdpSocket *socket;
+    QHostAddress server_ip; 
+};
+
+#endif // MYUDP_H
---
+++ streamcreator.h
@@ -0,0 +1,74 @@
+#include <ao/ao.h>
+#include <mpg123.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "stream.h"
+#include "sender.h"
+#define BITS 8
+class StreamCreator{
+	private:
+		mpg123_handle *mh;
+    	unsigned char *buffer;
+    	size_t buffer_size;
+    	size_t done;
+    	int err;
+
+    	int driver;
+    	ao_device *dev;
+
+    	ao_sample_format format;
+    	int channels, encoding;
+    	long rate;
+		StreamSender sender;
+	public:
+		StreamCreator(){
+			/* initializations */
+			ao_initialize();
+			driver = ao_default_driver_id();
+			mpg123_init();
+			mh = mpg123_new(NULL, &err);
+			buffer_size = mpg123_outblock(mh);
+			buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+		}
+	void openFile( char *argv[]){
+			/* open the file and get the decoding format */
+			mpg123_open(mh, argv[1]);
+			mpg123_getformat(mh, &rate, &channels, &encoding);
+
+			/* set the output format and open the output device */
+			format.bits = mpg123_encsize(encoding) * BITS;
+			format.rate = rate;
+			format.channels = channels;
+			format.byte_format = AO_FMT_NATIVE;
+			format.matrix = 0;
+			dev = ao_open_live(driver, &format, NULL);
+	}
+
+	void play(){
+		 /* decode and play */
+   
+		 while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK){
+        		ao_play(dev, buffer, done);
+			Stream s(channels, encoding, rate, buffer, buffer_size);
+			sender.sendStream(s);
+		}
+	}
+	~Player(){
+			/* clean up */
+			free(buffer);
+			ao_close(dev);
+			mpg123_close(mh);
+			mpg123_delete(mh);
+			mpg123_exit();
+			ao_shutdown();
+	}
+
+};
+
+ 	
+
+  
+
+   
+    
