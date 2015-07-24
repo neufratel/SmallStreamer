10a1cd8f1c207c38bb49c3d728cc8cae46af69a2
neufratel <rudzki.jaroslaw@gmail.com>
2015-07-24 09:41:55 +0200

    Spliting player on h cpp files adding volume control in second thread of player.

--- utils/stream.h
+++ utils/stream.h
@@ -2,16 +2,18 @@
 #define STREAM_H
 
 #include <iostream>
+#include <cstring>
 class Stream{
 	public:
 		static const size_t header=20;
 		int channels, encoding;
 		long rate;
+		int8_t  volume;
 		unsigned char* buf;
 		size_t length;
 		size_t buffer_size;
 		void print(){
-			std::cout<<"CH: "<<channels<<" ENC: "<<encoding<<" R: "<<rate<<" size: "<<buffer_size<<std::endl;
+			std::cout<<"V: "<<(short)volume<<" CH: "<<channels<<" ENC: "<<encoding<<" R: "<<rate<<" size: "<<buffer_size<<std::endl;
 		}
 		Stream(unsigned char*b, size_t len): buf(b), length(len){};
 		Stream();
@@ -27,12 +29,12 @@ class Stream{
 			encoding+=((int)init[6])<<8;
 			encoding+=((int)init[7]);
 
-			rate=	((long)init[8])<<56;
-			rate+=	((long)init[9])<<48;
-			rate+=	((long)init[10])<<40;
-			rate+=	((long)init[11])<<32;
-			rate+=	((long)init[12])<<24;
-			rate+=	((long)init[13])<<24;
+			volume=	((int8_t)init[8]);
+				(init[9]);
+				(init[10]);
+				(init[11]);
+			rate=	((long)init[12])<<24;
+			rate+=	((long)init[13])<<16;
 			rate+=	((long)init[14])<<8;
 			rate+=	((long)init[15])<<0;
 			
@@ -41,8 +43,8 @@ class Stream{
 			buffer_size+=((size_t)init[18])<<8;
 			buffer_size+=((size_t)init[19])<<0;	
 		}
-		Stream(int chan, int enc, long ra, size_t size, unsigned char * b):
-			channels(chan), encoding(enc), rate(ra), buffer_size(size)
+		Stream(int8_t vol, int chan, int enc, long ra, size_t size, unsigned char * b):
+			volume(vol), channels(chan), encoding(enc), rate(ra), buffer_size(size)
 			{
 				buf=new unsigned char[buffer_size];
 				memcpy ( buf, b, buffer_size );
@@ -61,10 +63,10 @@ class Stream{
 				ret[6]=(encoding>>8)  & 0xFF;
 				ret[7]=encoding 	  & 0xFF;
 				
-				ret[8]=(rate>>56) & 0xFF;
-				ret[9]=(rate>>48) & 0xFF;
-				ret[10]=(rate>>40) & 0xFF;
-				ret[11]=(rate>>32) & 0xFF;
+				ret[8]=volume;
+				ret[9]=0;
+				ret[10]=0;
+				ret[11]=0;
 				ret[12]=(rate>>24) & 0xFF;
 				ret[13]=(rate>>16) & 0xFF;
 				ret[14]=(rate>>8)  & 0xFF;
@@ -77,9 +79,8 @@ class Stream{
 				
 				for(int i=0; i<buffer_size; i++)
 					ret[20+i]=buf[i];
-				//memcpy ( ret+header, buf, buffer_size );
 
-		return ret;
+				return ret;
 	}
 	size_t getFrameSize(){ return (header+buffer_size);}
 			
